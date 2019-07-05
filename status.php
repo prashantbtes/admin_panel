bb956c8baf6716cbc0b4a539d7ca9dd3747947e7
prashantbtes <45783418+prashantbtes@users.noreply.github.com>
2019-07-05 10:03:33 +0530

    Add files via upload

---
+++ status.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Status Delivery Person </title>
+ <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <meta name="description" content="">
+   <meta name="author" content="">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+   <style type="text/css">
+   	.div
+   	style:position;
+
+   </style>
+
+
+</head>
+<body>
+<div class="container"><br><br><br>
+	<h2> Status: Available </h2>
+	<div class="div" style="position: relative;background: green; width: 100%; height:40px; "></div>
+ 	    <h2> Status: Not Available </h2>
+		<div class="div" style="position: relative; background:red; width:100%; height:40px; "></div>
+		<br>
+		 <br>
+		  <br>
+<center>
+  <h2> Status Of delivery Boy </h2></center>
+ <table class="table table-bordered">
+ 	<thead>
+ 		<tr>
+ 			<th scope="col">Id</th>
+ 			<th scope="col">Name</th>
+ 			<th scope="col">Mobile</th>
+ 			<th scope="col">Email</th>
+ 			<th scope="col">City</th>
+ 			<th scope="col">Availability</th>
+
+ 		</tr>
+<tbody>
+	<tr>
+		<td>1</td>
+		<td>Pk</td>
+		<td>987654321</td>
+		<td>email@gmail.com</td>
+		<td>mohali</td>
+		<td> <select class="table table-bordered col-md-3">
+		<option value="online">Available </option>
+		<option value="offline">Not Available </option>
+	    </select>
+	    </td>
+
+      
+	</tr>
+
+
+</tbody>
+
+ 
+
+
+
+
+</body>
+</html>
